cd90f40129b50b1b19fb5fae3d2d01540fb6ae3f
cristian <cdwenz@gmail.com>
2021-10-22 00:50:50 -0300

    with axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,11 @@
-import fetch from 'fetch'
 export const ADD_MOVIE_FAVORITE = "ADD_MOVIE_FAVORITE";
 export const GET_MOVIES = "GET_MOVIES";
 export const GET_MOVIE_DETAIL = "GET_MOVIE_DETAIL"
 export const REMOVE_MOVIE_FAVORITE = "REMOVE_MOVIE_FAVORITE"
+
 export function getMovies(titulo){
     return function(dispatch) {
-        return fetch("http://www.omdbapi.com/?apikey=a8dededa&s=" + titulo)
+        return axios("http://www.omdbapi.com/?apikey=a8dededa&s=" + titulo)
           .then(response => response.json())
           .then(json => {
             dispatch({ type: GET_MOVIES, payload: json });
@@ -15,7 +15,7 @@ export function getMovies(titulo){
 
 export function getMovieDetail(id){
     return function(dispatch) {
-        return fetch("http://www.omdbapi.com/?apikey=a8dededa&i=" + id)
+        return axios("http://www.omdbapi.com/?apikey=a8dededa&i=" + id)
           .then(response => response.json())
           .then(json => {
             dispatch({ type: GET_MOVIE_DETAIL, payload: json });
